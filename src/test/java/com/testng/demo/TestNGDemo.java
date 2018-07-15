a408c1f131cc2bf610ca164fd28854c3146d91d8
sachinboob <boob.sachin@gmail.com>
2018-07-15 22:30:45 +0530

    Added testng classes Added Bubble_Sort class

---
+++ src/test/java/com/testng/demo/TestNGDemo.java
@@ -0,0 +1,16 @@
+package com.testng.demo;
+
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+	@Test
+	public void method1() {
+		System.out.println("In method1");
+	}
+
+	@Test
+	public void method2() {
+		System.out.println("In method2");
+	}
+}
